d6f47e24d5629fd8866b84d511fd99b98add2391
MarwanELAdawy <marwan.mahmoud@cis.asu.edu.eg>
2017-12-19 20:59:59 +0200

    Registration

---
+++ Register/Controllers/AccountController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Register.Models;
+
+namespace Register.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+			using (Context db = new Context())
+			{
+				return View(db.userAccount.ToList());
+			}
+        }
+
+		public ActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Register(UserAccount account)
+		{
+			if (ModelState.IsValid)
+			{
+				using (Context db = new Context())
+				{
+					db.userAccount.Add(account);
+					db.SaveChanges();
+
+				}
+				ModelState.Clear();
+				ViewBag.Message = account.userName + " Successfuly Registered";
+			}
+			return View();
+		}
+
+		public ActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Login(UserAccount user)
+		{
+			using (Context db = new Context())
+			{
+				var usr = db.userAccount.Where(u => u.userName == user.userName && u.Password == user.Password).FirstOrDefault();
+				if(usr != null)
+				{
+					Session["UserID"] = usr.userId.ToString();
+					Session["UserName"] = usr.userName.ToString();
+					return RedirectToAction("LoggedIn");
+				}
+				else
+				{
+					ModelState.AddModelError("","UserName or Password is wrong");
+				}
+
+			}
+			return View();
+		}
+
+		public ActionResult LoggedIn()
+		{
+			if(Session["UserID"] !=null )
+			{
+				return View();
+			}
+			else
+			{
+				return RedirectToAction("Login");
+			}
+		}
+	}
+}
---
+++ Register/Models/UserAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Register.Models
+{
+	public class UserAccount
+	{
+		[Key]
+		public int userId { get; set; }
+		public string userName{ get; set; }
+		public string Password { get; set; }
+		public string Email { get; set; }
+
+
+	}
+}
